2d1a860f031c18254f79894a8c4c191e56fa45d8
Luiz miguel Macedo <126290507+LuizMacedo27@users.noreply.github.com>
2024-10-02 17:09:07 -0400

    Add files via upload

---
+++ a1.js
@@ -0,0 +1,20 @@
+console.log();
+console.log("MÉTODO PUSH");
+// MÉTODO PUSH
+let fruteira = ["Banana","Maçã","Pera","Uva"];
+fruteira.push("Abacaxi");
+console.log(fruteira);
+console.log();
+
+//MÉTODO SHIFT
+console.log("MÉTODO SHIFT");
+let cestaBasica = ["Arroz","Feijão","Café","Açucar"];
+cestaBasica.shift("Café")
+console.log(cestaBasica);
+console.log();
+
+console.log("MÉTODO SORT");
+//MÉTODO SORT
+let alfabeto = ["B","H","L","A","M"]
+alfabeto.sort();
+console.log(alfabeto);
